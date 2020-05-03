577c1c084aff62e23e6e7017a4b8e17fdb235ed8
HolidayMan <lesha.pitenko791@gmail.com>
2020-05-03 11:17:13 +0300

    added Category model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from pinker_backend.settings import MEDIA_ROOT
+
+
+class Category(models.Model):
+    image = models.ImageField(upload_to="images/")
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return f"<Category {self.name}>"
