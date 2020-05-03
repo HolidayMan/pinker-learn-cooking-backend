9350c37601084f5ecee51defab5a2dda8461483b
HolidayMan <lesha.pitenko791@gmail.com>
2020-05-03 14:43:41 +0300

    created DB

--- api/admin.py
+++ api/admin.py
@@ -5,3 +5,23 @@ from .models import *
 @admin.register(Category)
 class AdminCategory(admin.ModelAdmin):
     list_display = ['name', 'image']
+
+
+@admin.register(Measure)
+class AdminMeasure(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Ingredient)
+class AdminIngredient(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Dish)
+class AdminDish(admin.ModelAdmin):
+    list_display = ['name', 'image', 'category']
+
+
+@admin.register(DishIngredient)
+class AdminDishIngredient(admin.ModelAdmin):
+    list_display = ['dish', 'ingredient', 'measure', 'amount']
--- api/models.py
+++ api/models.py
@@ -9,4 +9,55 @@ class Category(models.Model):
     name = models.CharField(max_length=256, unique=True)
 
     def __str__(self):
-        return f"<Category {self.name}>"
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Dish(models.Model):
+    image = models.ImageField(upload_to="images/")
+    name = models.CharField(max_length=256, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Dish"
+        verbose_name_plural = "Dishes"
+
+
+class DishIngredient(models.Model):
+    dish = models.ForeignKey('Dish', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    measure = models.ForeignKey('Measure', on_delete=models.DO_NOTHING)
+    amount = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name = "Dish Ingredient"
+        verbose_name_plural = "Dish Ingredients"
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    dish = models.ManyToManyField('Dish', through="DishIngredient", related_name='ingredients')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Ingredient"
+        verbose_name_plural = "Ingredients"
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Measure"
+        verbose_name_plural = "Measures"
